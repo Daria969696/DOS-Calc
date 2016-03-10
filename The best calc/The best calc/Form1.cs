e04eee6743c7c7362649c21a4c90371b3d614e0c
buslaeva2016 <buslaeva_irinka96@mail.ru>
2016-03-10 13:56:13 +0600

    4 button
    Созданы 4 кнопки

--- The best calc/The best calc/Form1.cs
+++ The best calc/The best calc/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,38 @@ namespace The_best_calc
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(textBox1.Text);
+            double second = Convert.ToDouble(textBox2.Text);
+            double answer = first + second;
+            textBox3.Text = answer.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(textBox1.Text);
+            double second = Convert.ToDouble(textBox2.Text);
+            double answer = first-second;
+            textBox3.Text = answer.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(textBox1.Text);
+            double second = Convert.ToDouble(textBox2.Text);
+            double answer = first*second;
+            textBox3.Text = answer.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double first = Convert.ToDouble(textBox1.Text);
+            double second = Convert.ToDouble(textBox2.Text);
+            double answer = first/second;
+            textBox3.Text = answer.ToString();
         }
     }
 }
