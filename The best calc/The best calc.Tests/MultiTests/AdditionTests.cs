a237f5fbe31c00e641891d53a3650be9a9554005
daria969696 <daria969696@gmail.com>
2016-03-24 14:27:38 +0600

    Error
    Ошибка с тестами

---
+++ The best calc/The best calc.Tests/MultiTests/AdditionTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using The_best_calc.Multi;
+
+namespace The_best_calc.Tests.MultiTests
+{
+    [TestFixture]
+    public class AdditionTests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Addition calc = new Addition();
+            double result = calc.Action(5, 6);
+            Assert.Equals(11, result);
+        }
+    }
+}
